953f727e15d05a07b5facf6e2a25d971db248313
sleekydz86 <sleekydz86@icloud.com>
2023-01-13 22:45:06 +0900

    맥 깃커 알고리즘 테스트 마무리

--- Algorithm/src/com/javabook/algoritm1/Sample001.java
+++ Algorithm/src/com/javabook/algoritm1/Sample001.java
@@ -3,6 +3,6 @@ package com.javabook.algoritm1;
 public class Sample001 {
 	public static void main(String[] args) {
 		System.out.println("커밋돌아가나요???"); //맥에서 이클립스 커밋테스트입니다.
-		//알고리즘공부열심히..2`2
+		//알고리즘공부열심히하자.
 	}
 }
