6aa02a54cc1bfa9be89fab64e9f297fbd4c0e9ea
sleekydz86 <sleekydz86@icloud.com>
2023-01-13 22:29:57 +0900

    알고리즘테스트 오류나나요?

--- Algorithm/src/com/javabook/algoritm1/Sample001.java
+++ Algorithm/src/com/javabook/algoritm1/Sample001.java
@@ -2,6 +2,7 @@ package com.javabook.algoritm1;
 
 public class Sample001 {
 	public static void main(String[] args) {
-		System.out.println("커밋돌아가나요???"); //맥에서 이클립스 커밋테스트
+		System.out.println("커밋돌아가나요???"); //맥에서 이클립스 커밋테스트입니다.
+		//알고리즘공부열심히..
 	}
 }
