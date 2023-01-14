b3debac7c50f087a43ac264a943084040702686d
이경용 <sleekydz86@naver.com>
2023-01-14 15:03:11 +0900

    분모구하기
    분모구하기 1,4까지 확인

--- Algorithm/src/com/javabook/progmers/Sample004.java
+++ Algorithm/src/com/javabook/progmers/Sample004.java
@@ -4,7 +4,8 @@ public class Sample004 {
 	public static void main(String[] args) {
 		Solution4 s=new Solution4();
 		
-		int []result=s.solution(9,2,1,3);
+		//int []result=s.solution(9,2,1,3);
+		int []result=s.solution(1,2,3,4);
 		
 		for(int i=0; i<result.length; i++) {
 			System.out.println("결과값: "+result[i]);
@@ -16,7 +17,7 @@ class Solution4 {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int[] answer = new int[2];
         int footNumer=denom1*denom2;//분모
-        int topNumer=numer2*denom1+numer1*denom2;//분자
+        int topNumer=numer1*denom2+numer2*denom1;//분자
         int maxNum=1;//최소 공배수
         for(int i=1; i<=footNumer&&i<=topNumer; i++) { //약분
         	if(footNumer%i==0&&topNumer%i==0) {
