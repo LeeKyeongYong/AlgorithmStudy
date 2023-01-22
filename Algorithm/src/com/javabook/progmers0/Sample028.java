116b7527f0da3d1410f2e2b41267d96980097441
이경용 <sleekydz86@naver.com>
2023-01-23 03:11:15 +0900

    소스정리
    소스정리

--- Algorithm/src/com/javabook/progmers0/Sample028.java
+++ Algorithm/src/com/javabook/progmers0/Sample028.java
@@ -6,7 +6,6 @@ public class Sample028 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String x = "*";
         for(int i=0; i<n; i++) {
             for(int j=n; j>=n-i; j--) {
             	System.out.print("*");
