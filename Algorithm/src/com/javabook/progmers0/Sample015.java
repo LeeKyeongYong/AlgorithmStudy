cf7f2551b7d84d50e0411cb341773ef507321ed4
이경용 <sleekydz86@naver.com>
2023-01-22 21:36:24 +0900

    프로그래머스
    피자먹기+아이스커피

--- Algorithm/src/com/javabook/progmers0/Sample015.java
+++ Algorithm/src/com/javabook/progmers0/Sample015.java
@@ -1,5 +1,27 @@
 package com.javabook.progmers0;
 
-public class Sample015 {
+import java.util.Scanner;
 
+public class Sample015 {
+	public static void main(String[] args) {
+		Solution15 s=new Solution15();
+		System.out.print("몇명이 피자를 드실건가요? ");
+		Scanner sc=new Scanner(System.in);
+		int pisa=sc.nextInt();
+		int n=s.solution(pisa);
+		System.out.println("피자 몇판??: "+n);
+	}
+}
+class Solution15 {
+    public int solution(int n) {
+        int answer = 0;
+        int pan=n/7;
+        int moc=n%7;
+        if(moc==0) {
+            answer=pan*1;
+        } else {
+            answer=(pan*1+1);
+        }
+        return answer;
+    }
 }
--- Algorithm/src/com/javabook/progmers0/Sample016.java
+++ Algorithm/src/com/javabook/progmers0/Sample016.java
@@ -1,5 +1,25 @@
 package com.javabook.progmers0;
 
-public class Sample016 {
+import java.util.Scanner;
 
+public class Sample016 {
+	public static void main(String[] args) {
+		System.out.println("피자조각 말해주세요.");
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		Solution16 s=new Solution16();
+		int result=s.solution(n);
+		System.out.println("피자나눠먹을수있는 판은?? "+result);
+	}
+}
+class Solution16 {
+    public int solution(int n) {
+        int answer = 0;
+        int pize=6;
+        while(pize%n!=0) {
+        	pize+=6;
+        }
+        answer=pize/6;
+        return answer;
+    }
 }
