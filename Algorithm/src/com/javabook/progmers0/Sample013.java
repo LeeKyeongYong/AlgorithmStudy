d113107c10967467094f5c5faf94b5e77f872792
이경용 <sleekydz86@naver.com>
2023-01-22 20:22:30 +0900

    프로그래멋 문제 2개 풀기
    문제 풀기

--- Algorithm/src/com/javabook/progmers0/Sample013.java
+++ Algorithm/src/com/javabook/progmers0/Sample013.java
@@ -1,5 +1,51 @@
 package com.javabook.progmers0;
 
 public class Sample013 {
+	public static void main(String[] args) {
+		System.out.println("최소빈 값 구하기: ");
+		Solution13 s=new Solution13();
+		//int[] array= {1,2,3,3,4}; //결과값 3
+		//int[] array= {1,1,2,2}; //결과값 -1
+		int[] array= {1};
+		System.out.println(s.solution(array));
+	}
+}
+class Solution13 {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        int[] bin=new int[1001];
+        
+        int max=0;
+        
+        int m_idx=0;
+        
+        int cnt=0;
 
+    	if(array.length>0&&array.length<1000) { 
+    		
+	        for(int i=0; i<array.length; i++) {
+	        	bin[array[i]]++;
+	        }
+	        
+	        for(int i=0; i<bin.length; i++) {
+	        	if(bin[i]>max) {
+	        		max=bin[i];
+	            	m_idx=i;
+	        	}
+	        }
+	        
+	        for(int i=0; i<bin.length; i++) {
+	        	if(bin[i]==max)
+	        		cnt++;
+	        }
+	        
+	        if(cnt>1) {
+	        	answer=-1;
+	        } else {
+	        	answer=m_idx;
+	        }
+    	}
+        return answer;
+    }
 }
