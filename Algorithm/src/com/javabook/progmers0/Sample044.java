a5920a249984775924b2aed3bc3ac2442511b022
이경용 <sleekydz86@naver.com>
2023-01-29 20:07:01 +0900

    프로그래머스 9문제 풀기
    9문제 풀기

--- Algorithm/src/com/javabook/progmers0/Sample044.java
+++ Algorithm/src/com/javabook/progmers0/Sample044.java
@@ -1,7 +1,19 @@
 package com.javabook.progmers0;
 
-public class Sample044 {
+public class Sample044 { // ÀÚ¸®¼ö ´õÇÏ±â
 	public static void main(String[] args) {
-		
+		Solution44 s=new Solution44();
+		int result=s.solution(930211);
+		System.out.println("ÀÚ¸´¼ö µ¡¼À: "+result);
 	}
+}
+class Solution44 {
+    public int solution(int n) {
+        int answer = 0;
+        String[] result=Integer.toString(n).split("");
+        for(int i=0; i<result.length; i++) {
+        	answer+=Integer.parseInt(result[i]);
+        }
+        return answer;
+    }
 }
--- Algorithm/src/com/javabook/progmers0/Sample046.java
+++ Algorithm/src/com/javabook/progmers0/Sample046.java
@@ -1,7 +1,27 @@
 package com.javabook.progmers0;
 
-public class Sample046 {
+public class Sample046 {//구슬을 나누는 경우의 수
 	public static void main(String[] args) {
-		
+		Solution46 s=new Solution46();
+		int result=s.solution(5,3);
+		System.out.println("구술을 나누는 경우의 수: "+result);
 	}
+}
+
+class Solution46 {
+    public int solution(int balls, int share) {
+        int answer = 0;
+        answer=combin(balls,share);
+        return answer;
+    }
+    public int combin(int i,int j) {
+    	int result=0;
+    	if(j==0||i==j) {
+    		result=1;
+        } else {
+        	result=combin(i-1, j-1)+combin(i-1,j);
+        }
+    	return result;
+    }
+    
 }
