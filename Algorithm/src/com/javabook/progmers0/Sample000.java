33a35f2a4dfe05da0cbaeeabafb075792caac6aa
이경용 <sleekydz86@naver.com>
2023-01-29 13:07:17 +0900

    알고리즘 기초..
    겁나쉽다.

---
+++ Algorithm/src/com/javabook/progmers0/Sample000.java
@@ -0,0 +1,28 @@
+package com.javabook.progmers0;
+
+public class Sample000 {
+	public static void main(String[] args) {
+		/*
+		사람 a사람 = new 사람();
+		a사람.id = 20;
+		System.out.println("제 번호는 " + a사람.id + " 입니다.");
+		// 출력 : 제 번호는 20 입니다.
+		
+		for (int i=-100; i<26; i++){
+			System.out.println(i);
+		}
+		*/
+		int start=1;
+		
+		while(start<=10) {
+			int i=1;
+				while(i<4) {
+					  System.out.println(i);
+					  i++;
+					  
+					}
+		
+			start++;
+		}
+	}
+}
--- Algorithm/src/com/javabook/progmers0/Sample038.java
+++ Algorithm/src/com/javabook/progmers0/Sample038.java
@@ -1,5 +1,7 @@
 package com.javabook.progmers0;
 
 public class Sample038 {
-
+	public static void main(String[] args) {
+		
+	}
 }
