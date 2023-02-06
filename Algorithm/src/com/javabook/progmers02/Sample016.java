fd5c24bfbc2e1cee4f69bffacf3d2771f21cedd4
이경용 <sleekydz86@naver.com>
2023-02-06 17:02:44 +0900

    알고리즘 02월 06일 66문제 완료

--- Algorithm/src/com/javabook/progmers02/Sample016.java
+++ Algorithm/src/com/javabook/progmers02/Sample016.java
@@ -1,14 +1,39 @@
 package com.javabook.progmers02;
 
-public class Sample016 {
+public class Sample016 {//최대값 만들기
 	public static void main(String[] args) {
-		
+		int[] numbers= {0, -31, 24, 10, 1, 9};
+		Solution17 s=new Solution17();
+		int solution = s.solution(numbers);
+		System.out.println("최대값 만들기 result: "+solution);
 	}
 }
 
 class Solution17 {
     public int solution(int[] numbers) {
         int answer = 0;
+        
+        for(int X=0;X<numbers.length-1;X++){
+            for(int Y=X+1;Y<numbers.length;Y++){ //내림차순 정렬
+                if(numbers[X] < numbers[Y]){
+                    int C = numbers[X];
+                    numbers[X] = numbers[Y];
+                    numbers[Y] = C; 
+                }//if
+
+            }//for
+        }//for
+        
+        
+        int num1=numbers[0]*numbers[1];
+        int num2=numbers[numbers.length-1]*numbers[numbers.length-2];
+        
+        if(num1<num2) {
+        	answer = num2;
+        } else {
+        	answer = num1;
+        }
+        
         return answer;
     }
 }
