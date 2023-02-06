dd3e857ca374579be16c85b61d33480b7361e9f4
이경용 <sleekydz86@naver.com>
2023-02-06 14:18:12 +0900

    깃 크라켄으로 업로드

--- Algorithm/src/com/javabook/progmers02/Sample001.java
+++ Algorithm/src/com/javabook/progmers02/Sample001.java
@@ -2,6 +2,27 @@ package com.javabook.progmers02;
 
 public class Sample001 {
 	public static void main(String[] args) {
-		
+		int[]num_list = {1,2,3,4,5,6,7,8};
+		int n=2;
+		Solution001 s=new Solution001();
+		int[][] solution= s.solution(num_list, n);
+		for(int i=0; i<solution.length; i++) {
+			for(int j=0; j<solution[i].length; j++) {
+				System.out.print(solution[i][j]+" ");
+			}
+		}
+		System.out.println();
 	}
+}
+class Solution001 {
+    public int[][] solution(int[] num_list, int n) {
+    	
+    	   int[][] answer = {};
+           int length = num_list.length;
+           answer = new int[length/n][n];
+           for(int i=0; i<length; i++){  
+               answer[i/n][i%n]=num_list[i];   
+           }
+           return answer;
+    }
 }
--- Algorithm/src/com/javabook/progmers02/Sample002.java
+++ Algorithm/src/com/javabook/progmers02/Sample002.java
@@ -1,5 +1,18 @@
 package com.javabook.progmers02;
 
 public class Sample002 {
-
+	public static void main(String[] args) {
+		int[] numbers = {1,2,3,4};
+		int k=2;
+		Solution002 s=new Solution002();
+		int solution=s.solution(numbers,k);
+		System.out.println("result: "+solution);
+	}
+}
+class Solution002 {
+    public int solution(int[] numbers, int k) {
+        int answer = 0;
+        answer=numbers[2*(k-1)%numbers.length];
+        return answer;
+    }
 }
