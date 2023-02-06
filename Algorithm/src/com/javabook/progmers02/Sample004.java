72d4c6356f0da2fa4c582ad7ec47f4518c9acb3b
이경용 <sleekydz86@naver.com>
2023-02-06 15:41:07 +0900

    알고리즘 정리

--- Algorithm/src/com/javabook/progmers02/Sample004.java
+++ Algorithm/src/com/javabook/progmers02/Sample004.java
@@ -1,5 +1,18 @@
 package com.javabook.progmers02;
 
 public class Sample004 {
-
+	public static void main(String[] args) {
+		Solution004 s=new Solution004();
+		int[] box= {10,8,6};
+		int n=3;
+		int solution=s.solution(box, n);
+		System.out.println("result: "+solution);
+	}
+}
+class Solution004 {
+    public int solution(int[] box, int n) {
+        int answer = 0;
+        answer = (box[0]/n)*(box[1]/n)*(box[2]/n);
+        return answer;
+    }
 }
