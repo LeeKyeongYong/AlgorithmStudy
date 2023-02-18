1f179d1b057cfb03338c28d44b737225cff70c88
이경용 <sleekydz86@naver.com>
2023-02-18 19:14:51 +0900

    백준 코스튬 파티 정리

--- Algorithm/src/com/jababookbackjoon/Sample001.java
+++ Algorithm/src/com/jababookbackjoon/Sample001.java
@@ -1,7 +1,57 @@
 package com.jababookbackjoon;
 
-public class Sample001 { //백준문제 001
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Sample001 { //Q6159_코스튬파티
 	public static void main(String[] args) {
-		System.out.println("백준 12주차 첫번째문제");
+		 try {
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        StringBuilder sb = new StringBuilder();
+	        //선언 및 입력
+	        int N,S,cows[],cnt=0;
+	        int end=0;
+	       
+	        StringTokenizer st = new StringTokenizer(br.readLine());
+	        N = Integer.parseInt(st.nextToken());
+	        S = Integer.parseInt(st.nextToken());
+	        cows = new int[N];
+	        
+	        for(int i=0;i<N;i++)
+				
+					cows[i] = Integer.parseInt(br.readLine());
+			
+	        //정렬하기
+	        for(int i=0;i<cows.length-1;i++){
+	            
+	            for(int j=0; j<cows.length-1-i;j++){
+	                if(cows[j]>cows[j+1]){                   
+	                    int tmp = cows[j];
+	                    cows[j] = cows[j+1];
+	                    cows[j+1] = tmp;                  
+	                }              
+	            }          
+	        }
+	        
+	        
+		        for(int start = 0;start<N-1;start++) {
+		        	end = start+1;
+		        	if(cows[start]>=S)
+		        		break;
+		        	while(end <N && cows[start]+cows[end]<=S) {
+		        		cnt++;
+		        		end++;
+		        	}
+		        }
+		        System.out.println(cnt);
+	    	} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        
+	      
 	}
 }
