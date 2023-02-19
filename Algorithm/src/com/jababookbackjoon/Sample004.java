baff7cdc12b93c35945f7321bb4d7704876f1365
이경용 <sleekydz86@naver.com>
2023-02-19 17:02:54 +0900

    백준 겹치는것은 싫어 주몽 정리

--- Algorithm/src/com/jababookbackjoon/Sample004.java
+++ Algorithm/src/com/jababookbackjoon/Sample004.java
@@ -1,5 +1,31 @@
 package com.jababookbackjoon;
 
-public class Sample004 {
-
+import java.io.*;
+public class Sample004 { //Q2018_수들의합5
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int start=0,end=0;//투포인터 설정
+		int sum=0,cnt=0;//sum: 합,cnt: 가지수(정답)
+		
+		while(start<=N) { //end증가
+			while(++end<=N) { //부분합 증가
+				sum+=end;
+				if(sum>=N) {
+					if(sum==N) cnt++;
+					break;
+				}
+			}
+			while(++start<=N) {
+				sum-=start;
+				if(sum<=N) {
+					if(sum<=N) cnt++;
+					break;
+				}
+			}
+		}
+		
+		System.out.println(cnt);
+	}
 }
