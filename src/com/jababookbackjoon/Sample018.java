dfdcf82dcb928227c73cdeb439a296bd082cf7ab
이경용 <sleekydz86@icloud.com>
2023-05-17 22:17:36 +0900

    백준문제풀것...

---
+++ src/com/jababookbackjoon/Sample018.java
@@ -0,0 +1,19 @@
+package com.jababookbackjoon;
+
+public class Sample018 { // 백준 1261 알고스팟
+	static int n,m;
+	static String[] board;
+	static int[][]dist;
+	
+	public static void main(String[] args) {
+
+	}
+
+}
+/*
+<<입력>>
+3 3
+011
+111
+110
+*/
