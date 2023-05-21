a6f1173b22eddfa9b2d23bad1b0d0ec73b34a945
이경용 <sleekydz86@naver.com>
2023-05-21 21:16:04 +0900

    알고리즘 정리 20230521
    알고리즘 정리 20230521

--- src/com/jababookbackjoon/Sample032.java
+++ src/com/jababookbackjoon/Sample032.java
@@ -1,5 +1,67 @@
 package com.jababookbackjoon;
 
-public class Sample032 { //백준 1214번 쿨한 물건 구매
+import java.util.Scanner;
 
+public class Sample032 { //백준 1214번 쿨한 물건 구매
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		int D = sc.nextInt();
+		
+		int P = sc.nextInt();
+		
+		int Q = sc.nextInt();
+		
+		int ans;
+		
+		if(P<Q) {
+			int temp = P;
+			P = Q;
+			Q = temp;
+		}
+		
+		if(D%P == 0 || D%Q == 0) {
+			
+			System.out.println(D);
+			
+			return;
+			
+		} else {
+			ans = ( D / P )* P + P;
+		}
+		
+		int t=ans;
+		
+		for(int i=1; i<=t/P; i++) {
+			
+			int temp = t-P * i;
+			
+			if((D-temp)%Q==0) {
+				
+				System.out.println(D);
+				
+				return;
+				
+			} else {
+				temp +=((D - temp)/Q) * Q + Q;
+			}
+			
+			if(ans == temp)
+				break;
+			
+			ans = Math.min(temp, ans);
+			
+		}
+		
+		System.out.println(ans);
+		
+		sc.close();
+	}
 }
+
+/*
+입력>>
+17 7 13
+
+*/
--- src/com/jababookbackjoon/Sample037.java
+++ src/com/jababookbackjoon/Sample037.java
@@ -1,5 +1,29 @@
 package com.jababookbackjoon;
 
-public class Sample037 { //백준 16236 아기상어
+public class Sample037 { //백준 7662 이중 우선순위 큐
 
 }
+/*
+입력>>
+
+2
+7
+I 16
+I -5643
+D -1
+D 1
+D 1
+I 123
+D -1
+9
+I -45
+I 653
+D 1
+I -642
+I 45
+I 97
+D 1
+D -1
+I 333
+
+*/
