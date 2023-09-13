dfd370e370ae0cb14c920ba7d30c414f353b4480
이경용 <sleekydz86@icloud.com>
2023-09-13 22:59:57 +0900

    프로그래머스 기초 3문제 정리

---
+++ src/com/javabook/progmers03/Sample009.java
@@ -0,0 +1,16 @@
+package com.javabook.progmers03;
+
+public class Sample009 {//프로그래머스 A강조하기
+	public static void main(String[] args) {
+		String myString="abstract algebra";
+		Solution009 s= new Solution009();
+		String result = s.solution(myString);
+		System.out.println("A강조하기: "+result);
+	}
+}
+class Solution009 {
+	public String solution(String myString) {
+        String answer = myString.toLowerCase().replaceAll("a","A");
+        return answer;
+    }
+}
