2319efc71263d8a9cd697fa32e5a5d2437a61586
이경용 <sleekydz86@icloud.com>
2023-11-05 21:14:17 +0900

    깃 셋팅

---
+++ src/com/javabook/progmers03/Sample011.java
@@ -0,0 +1,12 @@
+package com.javabook.progmers03;
+
+public class Sample011 { //자연수 뒤집어 배열로 만들기
+
+}
+
+class Solution012{
+	public int[] solution(long n) {
+		int[] answer= {};
+		return answer;
+	}
+}
