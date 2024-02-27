0333596a95bd4b002c962d608977ea9df825f00b
이경용 <sleekydz86@icloud.com>
2024-02-27 12:20:12 +0900

    카카오 프렌즈 링크드연습

---
+++ src/com/algorithm2024/programers01/Exam20240227_launch.java
@@ -0,0 +1,91 @@
+package com.algorithm2024.programers01;
+
+public class Exam20240227_launch {//카카오 시뮬레이션 프렌즈4 블록2
+
+    public static int solution(int m, int n, String[] board) {
+        int answer = 0;
+
+        char[][] map = new char[m][n];
+        for (int i = 0; i < m; i++) {
+            map[i] = board[i].toCharArray();
+        }
+
+        while (true) {
+            boolean flag = false;
+            boolean[][] check = new boolean[m][n];
+
+            for (int i = 0; i < m - 1; i++) {
+                for (int j = 0; j < n - 1; j++) {
+                    if (map[i][j] != '.'
+                            && map[i][j] == map[i + 1][j]
+                            && map[i][j] == map[i + 1][j + 1]
+                            && map[i][j] == map[i][j + 1]
+                    ) {
+                        check[i][j] = true;
+                        check[i + 1][j] = true;
+                        check[i][j + 1] = true;
+                        check[i + 1][j + 1] = true;
+                        flag = true;
+                    }
+                }
+            }
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (check[i][j]) {
+                        map[i][j] = '.';
+                    }
+                }
+            }
+
+            // 3. 블럭을 터뜨리지 못하면 탈출
+            if (!flag) {
+                break;
+            }
+
+            // 2. 블럭 내리기
+            for (int j = 0; j < n; j++) {
+                Queue<Character> q = new LinkedList<>();
+
+                for (int i = m - 1; i >= 0; i--) {
+                    if (map[i][j] != '.') {
+                        q.add(map[i][j]);
+                    }
+                }
+
+                if (q.size() < m) {
+                    int needMore = m - q.size();
+
+                    for (int i = 0; i < needMore; i++) {
+                        q.add('.');
+                    }
+                }
+
+                for (int i = m - 1; i >= 0; i--) {
+                    map[i][j] = q.poll();
+                }
+            }
+        }
+
+        // 4. 맵에 있는 터진 공간을 카운트
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (map[i][j] == '.') {
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] board = {"TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"};
+        int m = 6;
+        int n = 6;
+
+        // 15
+        System.out.println(solution(m, n, board));
+    }
+
+}
