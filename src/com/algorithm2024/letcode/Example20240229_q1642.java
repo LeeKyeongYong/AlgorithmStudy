05037ffd67790f934d5ad3086d9f76735ad2a8ab
이경용 <sleekydz86@icloud.com>
2024-02-29 22:44:45 +0900

    그리디 힙 구현 클래스 완료

--- src/com/algorithm2024/letcode/Example20240229_q1642.java
+++ src/com/algorithm2024/letcode/Example20240229_q1642.java
@@ -1,4 +1,46 @@
 package com.algorithm2024.letcode;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class Example20240229_q1642 {
+
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        PriorityQueue<Integer> diff = new PriorityQueue<>(Comparator.reverseOrder());
+
+        int answer = 0;
+
+        int i = 0;
+        for (; i < heights.length - 1; i++) {
+            int step = heights[i + 1] - heights[i];
+
+            if (step <= 0) {
+                continue;
+            }
+
+            diff.add(step);
+            bricks -= step;
+
+            if (bricks < 0) {
+                bricks += diff.poll();
+                ladders--;
+            }
+
+            if (ladders < 0) {
+                break;
+            }
+        }
+
+        return i;
+    }
+
+    public static void main(String[] args) {
+        Example20240229_q1642 sol = new Example20240229_q1642();
+
+        // 4
+        System.out.println(sol.furthestBuilding(new int[]{4, 2, 7, 6, 9, 14, 12}, 5, 1));
+
+        // 7
+        System.out.println(sol.furthestBuilding(new int[]{4, 12, 2, 7, 3, 18, 20, 3, 19}, 10, 2));
+    }
 }
--- src/com/algorithm2024/letcode/Example20240229_q49.java
+++ src/com/algorithm2024/letcode/Example20240229_q49.java
@@ -1,4 +1,99 @@
 package com.algorithm2024.letcode;
 
+import java.util.*;
+
 public class Example20240229_q49 {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] temp = str.toCharArray();
+            Arrays.sort(temp);
+            String sortedStr = String.valueOf(temp);
+
+            map.putIfAbsent(sortedStr, new ArrayList<String>());
+            map.get(sortedStr).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    /**
+     * // FAIL(MEMORY) BACKTRACKS
+     *
+     *     static List<List<String>> answer;
+     *     static int n;
+     *     static boolean[] visited;
+     *
+     *     public List<List<String>> groupAnagrams(String[] strs) {
+     *         answer = new ArrayList<>();
+     *         n = strs.length;
+     *         visited = new boolean[n];
+     *
+     *         for (int i = 0; i < strs.length; i++) {
+     *             if (visited[i]) {
+     *                 continue;
+     *             }
+     *
+     *             String str = strs[i];
+     *             List<String> list = new ArrayList<>();
+     *             Map<Character, Integer> map = new HashMap<>();
+     *             for (char c : str.toCharArray()) {
+     *                 map.put(c, map.getOrDefault(c, 0) + 1);
+     *             }
+     *
+     *             list.add(str);
+     *             visited[i] = true;
+     *             permutation(str, list, strs, map);
+     *             answer.add(list);
+     *         }
+     *
+     *         return answer;
+     *     }
+     *
+     *     static void permutation(final String str, final List<String> list, final String[] strs, final Map<Character, Integer> map) {
+     *         for (int i = 0; i < n; i++) {
+     *             if (visited[i]) {
+     *                 continue;
+     *             }
+     *
+     *             String node = strs[i];
+     *             boolean isContinue = false;
+     *
+     *             HashMap<Character, Integer> copyMap = new HashMap<>(map);
+     *
+     *             for (char c : node.toCharArray()) {
+     *                 if (!copyMap.containsKey(c)) {
+     *                     isContinue = true;
+     *                     break;
+     *                 }
+     *
+     *                 copyMap.put(c, copyMap.get(c) - 1);
+     *                 if (copyMap.get(c) == 0) {
+     *                     copyMap.remove(c);
+     *                 }
+     *             }
+     *
+     *             if (isContinue) {
+     *                 continue;
+     *             }
+     *
+     *             if (copyMap.keySet().isEmpty()) {
+     *                 visited[i] = true;
+     *                 list.add(strs[i]);
+     *             }
+     *         }
+     *     }
+     */
+
+    public static void main(String[] args) {
+        Example20240229_q49 sol = new Example20240229_q49();
+
+        // Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+        System.out.println(sol.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+
+        // [["tittt","tttit","tttti"],["hhhhu","hhhuh","hhuhh"]]
+        System.out.println(sol.groupAnagrams(new String[]{"hhhhu", "tttti", "tttit", "hhhuh", "hhuhh", "tittt"}));
+    }
 }
