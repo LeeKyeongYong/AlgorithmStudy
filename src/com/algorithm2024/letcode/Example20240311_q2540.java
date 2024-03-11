08a6317fae72e6d262f6c0012e4938b4e7676e34
이경용 <sleekydz86@icloud.com>
2024-03-11 23:31:25 +0900

    이분탐색,시뮬레이션

--- src/com/algorithm2024/letcode/Example20240311_q2540.java
+++ src/com/algorithm2024/letcode/Example20240311_q2540.java
@@ -1,4 +1,21 @@
 package com.algorithm2024.letcode;
 
+import java.util.HashMap;
+
 public class Example20240311_q2540 {
+    public int getCommon(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i : nums1) {
+            map.put(i, 1);
+        }
+
+        for(int i : nums2) {
+            if(map.containsKey(i)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
 }
--- src/com/jababookbackjoon2/Exam20240311_q2169.java
+++ src/com/jababookbackjoon2/Exam20240311_q2169.java
@@ -1,4 +1,57 @@
 package com.jababookbackjoon2;
 
+import java.io.*;
+
 public class Exam20240311_q2169 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] inputs = br.readLine().split(" ");
+
+        int n = Integer.parseInt(inputs[0]);
+        int m = Integer.parseInt(inputs[1]);
+
+        int[][][] dp = new int[n + 1][m + 1][3];
+        int[][] arr = new int[n + 1][m + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                dp[i][j][0] = Integer.MIN_VALUE;
+                dp[i][j][1] = Integer.MIN_VALUE;
+                dp[i][j][2] = Integer.MIN_VALUE;
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            inputs = br.readLine().split(" ");
+
+            for (int j = 1; j <= m; j++) {
+                arr[i][j] = Integer.parseInt(inputs[j - 1]);
+            }
+        }
+
+        dp[1][1][0] = arr[1][1];
+        dp[1][1][1] = arr[1][1];
+        dp[1][1][2] = arr[1][1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (j > 1) {
+                    dp[i][j][0] = Math.max(dp[i][j - 1][0], dp[i][j - 1][1]) + arr[i][j];
+                }
+
+                if (i > 1) {
+                    dp[i][j][1] = Math.max(dp[i - 1][j][0], Math.max(dp[i - 1][j][1], dp[i - 1][j][2])) + arr[i][j];
+                }
+            }
+
+            for (int j = m - 1; j >= 1; j--) {
+                dp[i][j][2] = Math.max(dp[i][j + 1][1], dp[i][j + 1][2]) + arr[i][j];
+            }
+        }
+
+        bw.write(Math.max(dp[n][m][0], dp[n][m][1]) + "\n");
+        bw.flush();
+    }
 }
