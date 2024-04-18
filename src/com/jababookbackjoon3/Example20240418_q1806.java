1bf3397a47ab4b9fd2b46a5ec544c9aaf53555b2
이경용 <sleekydz86@icloud.com>
2024-04-18 19:59:54 +0900

    투포인터모음

---
+++ src/com/jababookbackjoon3/Example20240418_q1806.java
@@ -0,0 +1,39 @@
+package com.jababookbackjoon3;
+
+import java.util.Scanner;
+
+public class Example20240418_q1806 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int s = sc.nextInt();
+
+        int[] arr = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        int diff = Integer.MAX_VALUE;
+
+        while (left <= n && right <= n) {
+            if (sum < s) {
+                sum += arr[right];
+                right++;
+            } else {
+                diff = Math.min(diff, right - left);
+                sum -= arr[left];
+                left++;
+            }
+        }
+
+        if (diff == Integer.MAX_VALUE) {
+            System.out.println(0);
+        } else {
+            System.out.println(diff);
+        }
+    }
+}
