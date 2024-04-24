b40a983a0ec969edaa293e5cfd5ebac6deda6d2f
이경용 <sleekydz86@icloud.com>
2024-04-24 18:49:01 +0900

    람다식 사용해서 더 개선적으로 처리

---
+++ src/com/jababookbackjoon3/Example20240424_q12931_2.java
@@ -0,0 +1,28 @@
+package com.jababookbackjoon3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Example20240424_q12931_2 { //두배더하기 스트림겸 람다식이용
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(br.readLine());
+        int[] n = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int sum = Arrays.stream(n).sum();
+        int answer = 0;
+        while (sum != 0) {
+            int num = Arrays.stream(n).map(x -> x % 2).sum();
+            sum -= num;
+            answer += num;
+            n = Arrays.stream(n).map(x -> x / 2).toArray();
+            if (sum > 0) {
+                answer++;
+                sum /= 2;
+            }
+        }
+        System.out.print(answer);
+    }
+}
---
+++ src/com/jababookbackjoon3/Example20240424_q12931_3.java
@@ -0,0 +1,41 @@
+package com.jababookbackjoon3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Example20240424_q12931_3 { //두배더하기 간결하고 가독성 람다식 두번째 버전
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(br.readLine());
+        int[] n = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int sum = Arrays.stream(n).sum();
+        int answer = 0;
+
+        while (sum != 0) {
+            int num = (int) Arrays.stream(n)
+                    .filter(x -> x % 2 == 1)
+                    .map(x -> {
+                        x--;
+                        return 1;
+                    })
+                    .count();
+
+            if (num > 0) {
+                sum -= num;
+                answer += num;
+            } else {
+                sum /= 2;
+                answer++;
+                Arrays.setAll(n, x -> x / 2);
+            }
+        }
+
+        System.out.print(answer);
+    }
+}
