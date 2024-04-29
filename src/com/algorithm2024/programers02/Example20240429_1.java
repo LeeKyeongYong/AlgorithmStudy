dfcea5a3fff7cb6b831148ba73fcdcb750221459
이경용 <sleekydz86@icloud.com>
2024-04-29 14:33:06 +0900

    124 나라의 숫자

---
+++ src/com/algorithm2024/programers02/Example20240429_1.java
@@ -0,0 +1,20 @@
+package com.algorithm2024.programers02;
+
+public class Example20240429_1 {
+    public static String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            int remainder = n % 3;
+            if (remainder == 0) {
+                sb.insert(0, "4");
+                n = (n / 3) - 1;
+            } else {
+                sb.insert(0, remainder);
+                n /= 3;
+            }
+        }
+
+        String result = sb.toString().replaceAll("^0+", "");
+        return result.isEmpty() ? "1" : result;
+    }
+}
