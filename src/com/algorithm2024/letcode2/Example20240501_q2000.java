46f95c22a85c8e5a7eadde6d9bcaea85f84cdefd
이경용 <sleekydz86@icloud.com>
2024-05-01 21:04:13 +0900

    5월 백트래킹,그리디 문제 풀기 스터티 모인날..

---
+++ src/com/algorithm2024/letcode2/Example20240501_q2000.java
@@ -0,0 +1,20 @@
+package com.algorithm2024.letcode2;
+
+public class Example20240501_q2000 {
+    public static String reversePrefix(String word, char ch) {
+        if (!word.contains(Character.toString(ch))) {
+            return word;
+        }
+
+        int index = word.indexOf(ch);
+        StringBuilder rev = new StringBuilder(word.substring(0, index + 1)).reverse();
+
+        return rev.toString() + word.substring(index + 1);
+    }
+
+    public static void main(String[] args) {
+        // dcbaefd
+        System.out.println(reversePrefix("abcdefd", 'd'));
+    }
+}
+}
