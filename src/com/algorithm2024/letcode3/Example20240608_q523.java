37d1bab898fa9c15a71655d36f8209d25be443aa
이경용 <sleekydz86@icloud.com>
2024-06-08 22:21:21 +0900

    prefix 군간합 응용 -> re챌린지

--- src/com/algorithm2024/letcode3/Example20240608_q523.java
+++ src/com/algorithm2024/letcode3/Example20240608_q523.java
@@ -1,9 +1,11 @@
-package com.algorithm2024.letcode3
+package com.algorithm2024.letcode3;
 
-class Example20240608_q523 {
+import java.util.HashMap;
 
+public class Example20240608_q523 {
     public static boolean checkSubarraySum(int[] nums, int k) {
         // 0 23 25 31 35 42 // 6
+
         int prefix = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
 
@@ -29,9 +31,8 @@ class Example20240608_q523 {
     public static void main(String[] args) {
         // true
         System.out.println(checkSubarraySum(
-            new int[]{23, 2, 6, 4, 7},
-            6
+                new int[]{23, 2, 6, 4, 7},
+                6
         ));
     }
-
 }
