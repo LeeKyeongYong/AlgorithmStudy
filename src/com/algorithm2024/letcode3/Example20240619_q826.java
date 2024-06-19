478f75838776259f51bc187f779d86c365e2afea
이경용 <sleekydz86@icloud.com>
2024-06-19 22:02:03 +0900

    ReplaceWords 문제 다시풀이

--- src/com/algorithm2024/letcode3/Example20240619_q826.java
+++ src/com/algorithm2024/letcode3/Example20240619_q826.java
@@ -7,73 +7,34 @@ import java.util.List;
 
 public class Example20240619_q826 {
 
-    static class Node {
-        int diff;
-        int profit;
+    public String replaceWords(List<String> dictionary, String sentence) {
+        StringBuilder sb = new StringBuilder();
 
-        public Node(final int diff, final int profit) {
-            this.diff = diff;
-            this.profit = profit;
-        }
-    }
+        for (String s : sentence.split(" ")) {
+            boolean add = false;
+            int length = Integer.MAX_VALUE;
+            String toInsert = "";
 
-    public static int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        int answer = 0;
-
-        // 1. 작업 난이도 오름차순 정렬
-        List<Node> list = new ArrayList<>();
-        for (int i = 0; i < difficulty.length; i++) {
-            list.add(new Node(difficulty[i], profit[i]));
-        }
-        Collections.sort(list, (o1, o2) -> o1.diff - o2.diff);
-
-        // 2. 작업자 정렬
-        Arrays.sort(worker);
-
-        // 3. 리스트별의 작업량 별로 최대 수익량을 보장할 수 있도록 트릭 추가
-        int maxProfit = 0;
-        for (int i = 0; i < list.size(); i++) {
-            maxProfit = Math.max(maxProfit, list.get(i).profit);
-            list.get(i).profit = maxProfit;
-        }
-
-        // 4. 작업자의 작업량에 따른 최대 수익 찾기
-        for (int i = 0; i < worker.length; i++) {
-            int left = 0;
-            int right = list.size() - 1;
-            int target = worker[i];
-
-            while (left <= right) {
-                int mid = (left + right) / 2;
-                if (list.get(mid).diff <= target) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
+            for (String d : dictionary) {
+                if (s.startsWith(d) && d.length() <= length) {
+                    toInsert = d;
+                    length = d.length();
+                    add = true;
                 }
             }
 
-            if (right >= 0) {
-                answer += list.get(right).profit;
+            if (!add) {
+                sb.append(s + " ");
+            } else {
+                sb.append(toInsert + " ");
             }
         }
 
-        return answer;
-    }
+        if (sb.length() >= 2) {
+            sb.deleteCharAt(sb.length() - 1);
+        }
 
-    public static void main(String[] args) {
-        // 100
-        System.out.println(maxProfitAssignment(
-                new int[]{2, 4, 6, 8, 10},
-                new int[]{10, 20, 30, 40, 50},
-                new int[]{4, 5, 6, 7}
-        ));
-
-        // 324
-        System.out.println(maxProfitAssignment(
-                new int[]{68, 35, 52, 47, 86},
-                new int[]{67, 17, 1, 81, 3},
-                new int[]{92, 10, 85, 84, 82}
-        ));
+        return sb.toString();
     }
 
 }
