75bd36f031ba759080bf8ab0b3cc24190549286f
이경용 <sleekydz86@icloud.com>
2024-06-23 17:11:04 +0900

    prefixSum + 해싱

---
+++ src/com/algorithm2024/letcode2/Example20240613_q1248.java
@@ -0,0 +1,27 @@
+package com.algorithm2024.letcode2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Example20240613_q1248 {
+    public int numberOfSubarrays(int[] nums, int k) {
+        int subarrays = 0;
+        int nowSum = 0;
+
+        Map<Integer, Integer> prefixSum = new HashMap<>();
+        prefixSum.put(nowSum, 1);
+
+
+        for (int num : nums) {
+            nowSum += num % 2;
+
+            if (prefixSum.containsKey(nowSum - k)) {
+                subarrays += prefixSum.get(nowSum - k);
+            }
+
+            prefixSum.put(nowSum, prefixSum.getOrDefault(nowSum, 0) + 1);
+        }
+
+        return subarrays;
+    }
+}
