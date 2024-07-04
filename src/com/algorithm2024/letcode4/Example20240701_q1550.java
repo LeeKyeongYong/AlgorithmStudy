0626b2edbb3d30f4c58de2a647c3ba4bd8596607
이경용 <sleekydz86@icloud.com>
2024-07-04 15:58:35 +0900

    잘못 커밋된것 복원 처리

--- src/com/algorithm2024/letcode4/Example20240701_q1550.java
+++ src/com/algorithm2024/letcode4/Example20240701_q1550.java
@@ -1,17 +1,21 @@
-<div>
-    <div>상품 종류</div>
-    <div th:each="type : ${itemTypes}" class="form-check form-check-inline">
-        <input type="radio" th:field="*{itemType}" th:value="${type.name()}"
-        class="form-check-input">
-            <label th:for="${#ids.prev('itemType')}" th:text="${type.description}"
-            class="form-check-label">
-                    BOOK
-            </label>
-    </div>
-</div>
+package com.algorithm2024.letcode4;
 
+public class Example20240701_q1550 {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count = 0;
 
+        for (int i : arr) {
+            if (i % 2 == 1) {
+                count++;
+            } else {
+                count = 0;
+            }
 
+            if (count == 3) {
+                return true;
+            }
+        }
 
-
-
+        return false;
+    }
+}
