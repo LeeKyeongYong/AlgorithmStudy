bc353e3aa2c35a35f5ac80d9826337d46b354bca
이경용 <sleekydz86@naver.com>
2024-07-17 13:07:08 +0900

    feat: 리팩토링
    solution 메소드 내의 초기화 코드를 initialize 메소드로 분리하여 가독성 높이고
    코드 중복을 줄이기 위해 유효성 검사 부분을 isValid 메소드로 분리
    목표 지점에 도달했을 때 즉시 값을 반환 처리

--- src/com/algorithm2024/programers04/Example20240717_02.java
+++ src/com/algorithm2024/programers04/Example20240717_02.java
@@ -3,23 +3,18 @@ package com.algorithm2024.programers04;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Example20240717_02 { //리코쳇 로봇
+public class Example20240717_02 { // 리코쳇 로봇
 
-
-    static int n;
-    static int m;
+    static int n, m;
     static char[][] map;
-    static int[] start;
-    static int[] dest;
-    static int[][] pos = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    static int[] start, dest;
+    static final int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     static boolean[][] visited;
 
     static class Node {
-        int row;
-        int col;
-        int val;
+        int row, col, val;
 
-        public Node(final int row, final int col, final int val) {
+        Node(int row, int col, int val) {
             this.row = row;
             this.col = col;
             this.val = val;
@@ -27,20 +22,48 @@ public class Example20240717_02 { //리코쳇 로봇
     }
 
     public int solution(String[] board) {
-        int answer = 0;
-        boolean goal = false;
+        initialize(board);
 
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(start[0], start[1], 0));
+        visited[start[0]][start[1]] = true;
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+
+            if (current.row == dest[0] && current.col == dest[1]) {
+                return current.val;
+            }
+
+            for (int[] direction : directions) {
+                int newRow = current.row;
+                int newCol = current.col;
+
+                while (isValid(newRow + direction[0], newCol + direction[1]) && map[newRow + direction[0]][newCol + direction[1]] != 'D') {
+                    newRow += direction[0];
+                    newCol += direction[1];
+                }
+
+                if (!visited[newRow][newCol]) {
+                    visited[newRow][newCol] = true;
+                    queue.add(new Node(newRow, newCol, current.val + 1));
+                }
+            }
+        }
+        return -1;
+    }
+
+    private void initialize(String[] board) {
         n = board.length;
         m = board[0].length();
         map = new char[n][m];
+        visited = new boolean[n][m];
         start = new int[2];
         dest = new int[2];
-        visited = new boolean[n][m];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 map[i][j] = board[i].charAt(j);
-
                 if (map[i][j] == 'R') {
                     start = new int[]{i, j};
                 } else if (map[i][j] == 'G') {
@@ -48,55 +71,15 @@ public class Example20240717_02 { //리코쳇 로봇
                 }
             }
         }
+    }
 
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(start[0], start[1], 0));
-        visited[start[0]][start[1]] = true;
-        while (!q.isEmpty()) {
-            Node now = q.poll();
-
-            if (now.row == dest[0] && now.col == dest[1]) {
-                goal = true;
-                answer = now.val;
-                break;
-            }
-
-            for (int[] p : pos) {
-                int nr = now.row;
-                int nc = now.col;
-
-                while (nr >= 0 && nr < n && nc >= 0 && nc < m && map[nr][nc] != 'D') {
-                    nr += p[0];
-                    nc += p[1];
-                }
-
-                nr -= p[0];
-                nc -= p[1];
-
-                if (visited[nr][nc]) {
-                    continue;
-                }
-
-                visited[nr][nc] = true;
-                q.add(new Node(nr, nc, now.val + 1));
-            }
-
-        }
-
-        if (goal) {
-            return answer;
-        }
-
-        return -1;
+    private boolean isValid(int row, int col) {
+        return row >= 0 && row < n && col >= 0 && col < m;
     }
 
     public static void main(String[] args) {
-
         Example20240717_02 sol = new Example20240717_02();
-
         String[] board = {"...D..R", ".D.G...", "....D.D", "D....D.", "..D...."};
-
         System.out.println(sol.solution(board));
-
     }
 }
