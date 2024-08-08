d65ff60d1ee74d6c520ab86be9ce7c5ac240b6ae
이경용 <sleekydz86@naver.com>
2024-08-08 21:07:14 +0900

    TIL:Implement distinct string retrieval and character push minimization algorithms
    - Added method to retrieve the k-th distinct string from an array with duplicates removed.
    - Added method to calculate the minimum number of pushes needed for character mapping with varying costs based on the position.

---
+++ src/com/jababookbackjoon4/Example20240808_q3016.java
@@ -0,0 +1,49 @@
+package com.jababookbackjoon4;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class Example20240808_q3016 {
+    public int minimumPushes(String word) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        int lastFields = 8;
+        for (char c : word.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        // 오름차순 정렬
+        ArrayList<Character> keys = new ArrayList<>(map.keySet());
+        Collections.sort(keys, (o1, o2) -> map.get(o2) - map.get(o1));
+        int answer = 0;
+
+        for (int i = 0; i < keys.size(); i++) {
+            // 매핑이 가능한 경우
+            if (lastFields >= 1) {
+                lastFields--;
+                answer += map.get(keys.get(i));
+                continue;
+            }
+
+            // 매핑이 불가능한 경우
+            // 가장 키가 적은 곳에 세팅 (알파벳 수)
+            if (i >= 8 && i <= 15) {
+                answer += 2 * map.get(keys.get(i));
+            } else if (i >= 16 && i <= 23) {
+                answer += 3 * map.get(keys.get(i));
+            } else {
+                answer += 4 * map.get(keys.get(i));
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Example20240808_q3016 sol = new Example20240808_q3016();
+
+        // 24
+        System.out.println(sol.minimumPushes("aabbccddeeffgghhiiiiii"));
+    }
+}
