1a7343da2f399ad3f17889de4567718ad411da36
이경용 <sleekydz86@naver.com>
2024-08-09 21:41:21 +0900

    Add logic to filter out non-distinct elements and find the k-th distinct string

---
+++ src/com/algorithm2024/letcode5/Example20240809_q2053.java
@@ -0,0 +1,41 @@
+package com.algorithm2024.letcode5;
+
+import java.util.*;
+
+public class Example20240809_q2053 {
+
+    public String kthDistinct(String[] arr, int k) {
+        HashMap<String, Integer> map = new LinkedHashMap<>();
+
+        for (String s : arr) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
+
+        while (iterator.hasNext()) {
+            Map.Entry<String, Integer> entry = iterator.next();
+
+            if (entry.getValue() > 1) {
+                iterator.remove();
+            }
+        }
+
+        ArrayList<String> filtered = new ArrayList<>(map.keySet());
+        if (filtered.size() < k) {
+            return "";
+        }
+
+        return filtered.get(k - 1);
+    }
+
+    public static void main(String[] args) {
+        Example20240809_q2053 sol = new Example20240809_q2053();
+
+
+//        System.out.println(sol.kthDistinct(new String[]{"d", "b", "c", "b", "c", "a"}, 2));
+
+        System.out.println(sol.kthDistinct(new String[]{"a", "a"}, 1));
+    }
+
+}
