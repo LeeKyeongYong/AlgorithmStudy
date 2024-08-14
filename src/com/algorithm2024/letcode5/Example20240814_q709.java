a2a15662427382f003acbda7affc99a37e2d1e6e
이경용 <sleekydz86@naver.com>
2024-08-14 14:25:15 +0900

    Implement binary search for finding the k-th smallest distance pair
    - Added smallestDistancePair method to find the k-th smallest distance between pairs in a sorted array
    - Included helper method getDistanceUnderValueOfMid to count the number of pairs with distance <= mid
    - Added a main method for basic testing with example input

---
+++ src/com/algorithm2024/letcode5/Example20240814_q709.java
@@ -0,0 +1,48 @@
+package com.algorithm2024.letcode5;
+
+import java.util.Arrays;
+
+public class Example20240814_q709 {
+
+    public int smallestDistancePair(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int left = 0;
+        int right = nums[nums.length - 1] - nums[0];
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            int distance = getDistanceUnderValueOfMid(nums, mid);
+
+            if (distance < k) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+    private int getDistanceUnderValueOfMid(int[] nums, int mid) {
+        int count = 0;
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            while (nums[right] - nums[left] > mid) {
+                left++;
+            }
+
+            count += (right - left);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Example20240814_q709 sol = new Example20240814_q709();
+        System.out.println(sol.smallestDistancePair(new int[]{1, 6, 1}, 3));
+    }
+
+}
