b9a6940c36f8b8aa76a79bc4d66daf7be808afeb
이경용 <sleekydz86@icloud.com>
2024-09-03 02:11:47 +0900

    feat:Implement chalk replacer algorithm to find the student who will use up the chalk
    Added an initial implementation for the chalk replacer problem. The first method iteratively subtracts chalk from k and loops through students until the remaining chalk is insufficient for the current student. The second method optimizes this by calculating the total sum and using modulo operations to determine the remaining chalk, significantly improving efficiency.

--- src/com/algorithm2024/letcode6/Example20240903_q1894.java
+++ src/com/algorithm2024/letcode6/Example20240903_q1894.java
@@ -1,2 +1,55 @@
-package com.algorithm2024.letcode6;public class Example20240903_q1894 {
+package com.algorithm2024.letcode6;
+
+public class Example20240903_q1894 {
+    public int chalkReplacer(int[] chalk, int k) {
+        int index = 0;
+        while (true) {
+            if (index >= chalk.length) {
+                index = 0;
+            }
+
+            if (chalk[index] > k) {
+                break;
+            }
+
+            k -= chalk[index];
+
+            index++;
+        }
+
+        return index;
+    }
+
+    public int chalkReplacer2(int[] chalk, int k) {
+        int n = chalk.length;
+
+        long[] prefixSum = new long[n];
+        prefixSum[0] = chalk[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + chalk[i];
+        }
+
+        long sum = prefixSum[n - 1];
+        long remainingChalk = (k % sum);
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            int need = chalk[i];
+
+            if (remainingChalk < need) {
+                answer = i;
+                break;
+            }
+
+            remainingChalk -= need;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Example20240903_q1894 sol = new Example20240903_q1894();
+
+        System.out.println(sol.chalkReplacer2(new int[]{5, 1, 5}, 22));
+    }
 }
