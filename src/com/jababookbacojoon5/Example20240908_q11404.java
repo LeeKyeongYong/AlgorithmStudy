6ced3d55f1dea3ade45514c8dedc5834e4bd3275
이경용 <sleekydz86@naver.com>
2024-09-08 15:01:29 +0900

    feat: Added Floyd-Warshall algorithm for path calculation
    - Implemented shortest path calculation logic using the Floyd-Warshall algorithm
    - Applied the algorithm to two different problem-solving cases for calculating paths based on input map data

---
+++ src/com/jababookbacojoon5/Example20240908_q11404.java
@@ -0,0 +1,57 @@
+package com.jababookbacojoon5;
+
+import java.util.Scanner;
+
+public class Example20240908_q11404 {
+
+    private static final int INF = 100000001;
+
+    static int n;
+    static int m;
+    static int[][] map;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+        map = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    map[i][j] = 0;
+                } else {
+                    map[i][j] = INF;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            int value = sc.nextInt();
+            map[start][end] = Math.min(map[start][end], value);
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (map[i][j] == INF) {
+                    System.out.print(0 + " ");
+                } else {
+                    System.out.print(map[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ src/com/jababookbacojoon5/Example20240908_q17182.java
@@ -0,0 +1,60 @@
+package com.jababookbacojoon5;
+
+import java.util.Scanner;
+
+public class Example20240908_q17182 {
+
+    static int n;
+    static int start;
+    static int[][] map;
+    static boolean[] visited;
+    static int answer;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        start = sc.nextInt();
+        map = new int[n][n];
+        visited = new boolean[n];
+        answer = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                map[i][j] = sc.nextInt();
+            }
+        }
+
+        floydWarshall();
+
+        visited[start] = true;
+        dfs(1, start, 0);
+        System.out.println(answer);
+    }
+
+    static void floydWarshall() {
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+                }
+            }
+        }
+    }
+
+    static void dfs(int count, int now, int sum) {
+        if (count == n) {
+            answer = Math.min(answer, sum);
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs(count + 1, i, sum + map[now][i]);
+                visited[i] = false;
+            }
+        }
+    }
+
+}
