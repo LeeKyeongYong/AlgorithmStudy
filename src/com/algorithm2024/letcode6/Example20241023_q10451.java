6066e639f25af852d3e29d2036ba483c920d32c7
이경용 <sleekydz86@naver.com>
2024-10-23 23:48:42 +0900

    feat: Calculate maximum OR subsets using bitwise operations and resolve DFS cycles
    - Implemented an algorithm to calculate the number of subsets with the maximum OR value using bitwise operations
    - Solved the cycle detection problem using DFS
    - Marked node visits in the code to prevent duplicates

--- src/com/algorithm2024/letcode6/Example20241023_q10451.java
+++ src/com/algorithm2024/letcode6/Example20241023_q10451.java
@@ -1,2 +1,59 @@
-package com.algorithm2024.letcode6;public class Example20241023_q2044 {
+package com.algorithm2024.letcode6;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Example20241023_q10451 {
+
+    static final StringBuilder sb = new StringBuilder();
+    static final Scanner sc = new Scanner(System.in);
+    static ArrayList<Integer>[] list;
+    static boolean[] visited;
+
+    public static void main(String[] args) {
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            solve();
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    static void solve() {
+        int n = sc.nextInt();
+
+        list = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i <= n; i++) {
+            int num = sc.nextInt();
+            list[i].add(num);
+        }
+
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                count++;
+                dfs(i);
+            }
+        }
+
+        sb.append(count).append("\n");
+    }
+
+    static void dfs(int node) {
+        visited[node] = true;
+
+        for (Integer i : list[node]) {
+            if (!visited[i]) {
+                dfs(i);
+            }
+        }
+    }
+
 }
