6999de634b4d9427b6cbcaa88c3129e04bd41f8a
이경용 <sleekydz86@naver.com>
2024-11-12 23:25:46 +0900

    feat:Implement logic to count teams in increasing and decreasing order based on ratings

---
+++ src/com/algorithm2024/letcode7/Example20241112_qresolve.java
@@ -0,0 +1,47 @@
+package com.algorithm2024.letcode7;
+
+public class Example20241112_qresolve {
+
+    public int numTeams(int[] rating) {
+        int answer = 0;
+
+        for (int boundary = 0; boundary < rating.length; boundary++) {
+            int leftSamller = 0;
+            int leftLager = 0;
+            int rightSmaller = 0;
+            int rightLager = 0;
+
+            for (int left = 0; left < boundary; left++) {
+                if (rating[left] < rating[boundary]) {
+                    leftSamller++;
+                } else {
+                    leftLager++;
+                }
+            }
+
+            for (int right = boundary + 1; right < rating.length; right++) {
+                if (rating[boundary] > rating[right]) {
+                    rightSmaller++;
+                } else {
+                    rightLager++;
+                }
+            }
+
+            // 오름차순 기준 팀 수 : 기준수보다 작은 수 * 기준수보다 큰 수 (팀의 수는 3이므로)
+            answer += (leftSamller * rightLager) + (leftLager * rightSmaller);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Example20241112_qresolve sol = new Example20241112_qresolve();
+
+        // 3
+        System.out.println(sol.numTeams(new int[]{2, 5, 3, 4, 1}));
+
+        // 0
+        System.out.println(sol.numTeams(new int[]{2, 1, 3}));
+    }
+
+}
